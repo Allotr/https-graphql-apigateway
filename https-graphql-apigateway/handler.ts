cf1a7f929607a1d73c77170b26bc83e5c43c778f
rafaelpernil2 <mysterysynthman@gmail.com>
2023-08-28 00:17:32 +0200

    fix: simplify logic

--- https-graphql-apigateway/handler.ts
+++ https-graphql-apigateway/handler.ts
@@ -6,12 +6,13 @@ import { createYoga } from "graphql-yoga";
 import { TemplatedApp } from "uWebSockets.js";
 import { useGraphQlJit } from '@envelop/graphql-jit'
 import { useParserCache } from "@envelop/parser-cache";
+import cookie from "cookie";
 
 import { useResponseCache, UseResponseCacheParameter } from '@graphql-yoga/plugin-response-cache'
 import { createRedisCache } from '@envelop/response-cache-redis'
-import { getUserInfoFromRequest, initializeSessionStore, logoutSession } from "./src/middlewares/auth";
+import { initializeSessionStore } from "./src/middlewares/auth";
 import { corsRequestHandler } from "./src/middlewares/cors";
-import { ServerContext, UserContext } from "./src/types/yoga-context";
+import { ServerContext } from "./src/types/yoga-context";
 import { queryNames } from "./src/consts/query-names";
 import _ from "lodash";
 
@@ -23,24 +24,8 @@ function onServerCreated(app: TemplatedApp) {
   const cache = createRedisCache({ redis }) as UseResponseCacheParameter["cache"]
   initializeSessionStore();
 
-  const yoga = createYoga<ServerContext, UserContext>({
+  const yoga = createYoga<ServerContext>({
     schema: makeGatewaySchema(),
-    context: async ({ req, res, request }) => {
-      // Context factory gets called for every request
-      const [sid, userId] = await getUserInfoFromRequest(request);
-      return {
-        req,
-        res,
-        user: {
-          _id: userId! // After login the user id is not null
-        },
-        mongoDBConnection: getMongoDBConnection(),
-        redisConnection: getRedisConnection(),
-        sid: sid!, // After login the session id is not null
-        logout: logoutSession,
-        cache
-      }
-    },
     cors: corsRequestHandler,
     graphiql: true,
     plugins: [
@@ -48,9 +33,10 @@ function onServerCreated(app: TemplatedApp) {
       useParserCache(),
       useResponseCache({
         idFields: ["id", "_id"],
-        session: async (request) => {
-          const [sid] = await getUserInfoFromRequest(request);
-          return sid
+        session: (request) => {
+          const cookieList = request.headers.get('cookie') ?? "";
+          const parsedCookie = cookie.parse(cookieList);
+          return parsedCookie?.['connect.sid'];
         },
         shouldCacheResult: ({ result }) => {
           const functionBlacklist = [
